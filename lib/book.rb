170b235299d75c7d7c89407664dd44bcc554a4b6
Kyle Ortiz <kyleaortiz@gmail.com>
2021-09-08 19:51:50 -0500

    commit tree e678b2be2f34a195ad8c649935c7f61a39dc31d2

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book
+     def initialize(title)
+          @title = title
+     end
 
+     attr_accessor :page_count, :title, :author, :genre
+
+     def turn_page()
+          puts "Flipping the page...wow, you read fast!"
+     end
+end
 
